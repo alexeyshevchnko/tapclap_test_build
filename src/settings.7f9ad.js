4a6336ad9aa5edb3945b5c40dd169663ed70e0f6
alexeyshevchenko88 <alexeyshevchenko88@gmail.com>
2025-08-11 13:07:37 +0300

    билд

---
+++ src/settings.7f9ad.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/main - 001.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d17e2",main:"03c70"}};
